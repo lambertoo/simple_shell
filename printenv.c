1f4796ffe060ab7de099768de0b0029b8d053ede
lambertoo <rulindanal@gmail.com>
2023-11-15 11:44:49 -0800

    staring from scratch

--- printenv.c
+++
@@ -1,9 +0,0 @@
-#include "main.h"
-
-/**
- * print_env - Print the environment variables.
- */
-void print_env(void)
-{
-    /* Your print_env function code here */
-}
--- prompt.c
+++
@@ -1,9 +0,0 @@
-#include "main.h"
-
-/**
- * print_prompt - Print the shell prompt.
- */
-void print_prompt(void)
-{
-    /* Your print_prompt function code here */
-}
